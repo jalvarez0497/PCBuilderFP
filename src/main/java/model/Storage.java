7783b3f7de387770634d819ab78d46bdb37b2964
Jean Alvarez <jean.alvarez0497@gmail.com>
2026-04-05 22:57:08 -0500

    first commit

---
+++ src/main/java/model/Storage.java
@@ -0,0 +1,19 @@
+/** Program description
+ * @author [Jean M. Alvarez Robles] 
+ * @version 1.0
+ * @since 2026
+*/
+/*  
+* OS: [Mac]
+* IDE: [IntelliJ]
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified, nor used generative AI as a final draft. 
+* I have not given other fellow student(s) access to my program.
+*/
+package model;
+
+public class Storage {
+}
